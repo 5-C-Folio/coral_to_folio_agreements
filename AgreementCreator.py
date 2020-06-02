84a990bbf8f22e86cbdb7dd35f259bc88e2dd15b
Aaron Neslin <aneslin125@gmail.com>
2020-06-02 10:52:25 -0400

    initial commit

---
+++ AgreementCreator.py
@@ -0,0 +1,99 @@
+import folio_api
+import json
+from csv import DictReader
+from uuid import uuid4
+from datetime import datetime
+
+def string_to_dict(string):
+    #smith uses , to split- umass ;
+    sp_string = string.split(',')
+    altList = []
+    for row in sp_string:
+        sprow = row.split(':')
+        altname = {"value" : sprow[1].lstrip(), "description":sprow[0]}
+        altList.append(altname)
+    return altList
+
+class agreement():
+    def __init__(self,**kwargs):
+
+        self.coralID = None
+        self.orgCode = None
+        self.name = None
+        self.description = None
+        self.startDate = None
+        self.endDate = None
+        self.isPerpetual = None
+        self.renewalPriority = None
+        self.orgs = []
+        self.periods = []
+        self.alias = None
+        self.periodNote = None
+        self.__dict__.update(kwargs)
+
+    def orgGetter(self, orgObject):
+        # query the org object for a uuid
+        self.org = {"org": {"orgsUuid": orgObject.get("UM" + self.orgCode)}, "role": "content_provider"}
+        self.orgs.append(self.org)
+
+
+    def serialize(self):
+        if self.endDate is not None and len(self.endDate) > 1:
+            self.periods.append({"startDate": datetime.strptime(self.startDate,"%m/%d/%Y"), "endDate": self.endDate.date()})
+        else:
+            self.periods.append({"startDate": str(datetime.strptime(self.startDate,"%m/%d/%Y").isoformat()), "note": self.periodNote})
+
+        if self.alias is not None:
+            self.alias = string_to_dict(self.alias)
+
+        self.id = str(uuid4())
+
+        return json.dumps({
+          "id" : self.id,
+          "name": self.name,
+          "description": self.description,
+          "periods" : self.periods,
+          "isPerpetual": self.isPerpetual,
+          "renewalPriority": self.renewalPriority,
+          "orgs" : self.orgs,
+          "agreementStatus": "Active"
+
+        }, indent = 4)
+
+
+class note():
+
+    def __init__(self, type, title, content, linkID):
+        self.domain = 'agreements'
+        self.title = title
+        self.content=content
+        self.links = [{"id" : linkID, "type" : "agreement" }]
+        self.type = type
+
+    def typeGetter(self,typeObject):
+        self.typeID = typeObject.get(self.type)
+
+    def serialize(self):
+        return json.dumps(self, default=lambda o : o.__dict__)
+
+
+
+
+
+if __name__ == "__main__":
+    orgObject = folio_api.organization('https://okapi-fivecolleges.folio.ebsco.com/', 'tennent')
+    orgObject.getToken('userName', 'password')
+    orgObject.get_orgs('UM')
+    orgObject.get_noteTypes()
+    with open ("orgs/Coral/agreements/umResources3.csv" ,"r", encoding='utf8') as coralResource:
+        coralDict = DictReader(coralResource)
+        for row in coralDict:
+            workingAgreement = agreement(coralID = row['resourceID'],
+                                         orgCode = row['orgCode'],
+                                         name = row['titleText'],
+                                         description = row['descriptionText'],
+                                         startDate = row['currentStartDate'],
+                                         endDate = row['currentEndDate'],
+                                         periodNote = "order#: " +row["orderNumber"] + ", system#: " + row['systemNumber'])
+            workingAgreement.orgGetter(orgObject.org_key)
+            print (workingAgreement.serialize())
---
+++ folio_api.py
@@ -0,0 +1,59 @@
+import requests
+import json
+
+
+class requestObject():
+    def __init__(self, url, tenant):
+        if url[-1] == '/':
+            self.url = url
+        else:
+            self.url = url + '/'
+        self.tenant = tenant
+        self.token = None
+    def getToken(self, userName, Password):
+
+        headers = {'Content-Type': 'application/json',
+                   'x-okapi-tenant': self.tenant}
+        payload = {"username" : userName,
+                   "password": Password}
+        connection_url = self.url + "authn/login"
+        login = requests.post(connection_url, headers=headers, data=json.dumps(payload))
+
+        try:
+            self.token = login.headers['x-okapi-token']
+
+        except KeyError:
+            print (login.text)
+
+class organization(requestObject):
+
+    def get_orgs(self, orgCode):
+        if self.token is None:
+            print("get a token!")
+            exit()
+        headers = {'Content-Type': 'application/json', 'x-okapi-tenant': self.tenant, 'x-okapi-token': self.token}
+        r_url = f'{self.url}organizations-storage/organizations?query=code=="{orgCode}*"&limit=1200'
+        orgs = requests.get(r_url, headers = headers, timeout= .5)
+        json_response = json.loads(orgs.text)
+        self.org_key = {}
+        for results in json_response['organizations']:
+
+            self.org_key[results["code"]] = results['id']
+
+    def get_noteTypes(self):
+        if self.token is None:
+            print("get a token!")
+            exit()
+        headers = {'Content-Type': 'application/json', 'x-okapi-tenant': self.tenant, 'x-okapi-token': self.token}
+        r_url = f'{self.url}note-types?limit=100'
+        noteTypes = requests.get(r_url, headers = headers, timeout= .5)
+        json_response = json.loads(noteTypes.text)
+        self.noteTypes = {}
+        for results in json_response['noteTypes']:
+            self.noteTypes[results["name"]] = results['id']
+
+if __name__ == '__main__':
+    x = organization('https://okapi-fivecolleges.folio.ebsco.com/', 'tenant')
+    x.getToken('userName', 'Password')
+    print (x.token)
+    x.get_orgs('UM')
